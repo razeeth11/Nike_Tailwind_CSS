4f0eac69064ea3c5b0b97f03f3a6c4ec63c7cea2
razeeth11 <razeeth011@gmail.com>
2023-09-04 21:13:37 +0530

    Grocery-react-tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,46 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      
+    <div className="App min-h-screen w-full bg-[url('src/assets/images/Image.png')] bg-center bg-cover">
+      <nav>
+        <div className="nav-bar flex items-center justify-between px-[20px] py-[15px] w-full">
+          <img
+            className="cursor-pointer"
+            src="src\assets\images\logo.png"
+            alt="logo.png"
+            width="200px"
+          />
+          <ul className="flex gap-[60px] items-center text-[14px]">
+            <li className=" hover:text-yellow-400">
+              <a href="#">Home</a>
+            </li>
+            <li className=" hover:text-yellow-400">
+              <a href="#">About</a>
+            </li>
+            <li className=" hover:text-yellow-400">
+              <a href="#">Features</a>
+            </li>
+            <li className=" hover:text-yellow-400">
+              <a href="#">Contact</a>
+            </li>
+            <span class="material-symbols-outlined hover:text-yellow-400 cursor-pointer">
+              local_mall
+            </span>
+          </ul>
+        </div>
+      </nav>
+      <main>
+        <div className="flex items-center px-5">
+          <div className="marketing-line">
+          <h1 className="text-[48px] font-bold">Groceries <br/> Delivery in <span className="text-yellow-400">15mins</span></h1>
+          <p className="text-[14px] mt-5 leading-[30px]">Grocify offer a wide range of products, including fresh products, meats, dairy, baked goods and non-perishable items.</p>
+          <button className="mt-10 px-5 py-3 bg-yellow-400 text-blue-950 rounded font-medium">Shop Now</button>
+          </div>
+          <div className="marketing-image">
+            <img src="src\assets\images\grocery-image.png" alt="marketing-image.png" width='700px'/>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
-
